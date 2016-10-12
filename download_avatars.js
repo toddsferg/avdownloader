2c666920af89df36966fdcade2568caad683768b
Todd Ferguson <toddsferg@gmail.com>
2016-10-12 14:14:44 -0700

    cleaning up code

--- download_avatars.js
+++ download_avatars.js
@@ -1,9 +1,7 @@
 var request = require('request');
 var fs = require('fs');
-var myArgs = process.argv.slice(2);
-//getRepoContributors(myArgs[0], myArgs[1], downloadByURL);
-getRepoContributors("lighthouse-labs", "laser_shark", downloadByURL);
 
+getRepoContributors("lighthouse-labs", "laser_shark", downloadByURL);
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var endpoint = 'https://api.github.com/repos/' + repoOwner + '/' + repoName + "/contributors?access_token=f8b6234339b00d1b44045f1a46902289a689cf0a"
@@ -22,6 +20,6 @@ function getRepoContributors(repoOwner, repoName, cb) {
    })
 }
 
-function downloadByURL(url,filePath){
+function downloadByURL(url, filePath){
   request(url).pipe(fs.createWriteStream(filePath));
 }
